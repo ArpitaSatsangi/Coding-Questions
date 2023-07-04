49ef01d25123d35074ad81686b51d515e849c25b
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-07-04 17:14:23 +0530

    Creat 10. Sleep deprivation.cpp

---
+++ 10. Sleep deprivation.cpp
@@ -0,0 +1,29 @@
+/*
+Problem:-
+A person is said to be sleep deprived if he slept strictly less than 7 hours in a day.
+Chef was only able to sleep X hours yesterday. Determine if he is sleep deprived or not.
+*/
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x;
+	    cin>>x;
+	    
+	    if(x<7)
+	    cout<<"YES"<<endl;
+	    
+	    else
+	    cout<<"NO"<<endl;
+	}
+	
+	return 0;
+}
