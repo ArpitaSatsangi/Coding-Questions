c4e1a5850978ed4c4247b4f2e4904db263b882ee
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-07-04 17:55:33 +0530

    Create

---
+++ 15. Coldplay.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+# include <math.h>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int m,s;
+	    cin>>m>>s;
+	    
+	    cout<<floor(m/s)<<endl;
+	}
+	
+	return 0;
+}
