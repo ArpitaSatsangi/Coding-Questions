ead17eb5f9d0ac80000433e352ad26fa931015be
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-08-07 07:48:44 -0700

    Create 42. Friends Meetup Difficulty Rating:1098.cpp

---
+++ 42. Friends Meetup Difficulty Rating:1098.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int x1,x2,i=0,count=0;
+	    cin>>x1>>x2;
+
+	    
+	    if(x2-x1<1)
+	    cout<<"YES\n";
+	    
+	    else
+	    cout<<"NO\n";
+	}
+	
+	return 0;
+}
