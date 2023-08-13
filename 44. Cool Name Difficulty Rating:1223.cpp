96c599bd53d9776ef8c9d0ea370e660a65ea6ef9
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-08-13 09:25:18 -0400

    Update 44. Cool Name Difficulty Rating:1223.cpp

--- 44. Cool Name Difficulty Rating:1223.cpp
+++ 44. Cool Name Difficulty Rating:1223.cpp
@@ -1,8 +1,8 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main() {
-	// your code goes here
+	
 	int t;
 	cin>>t;
 	
@@ -11,45 +11,19 @@ int main() {
 	    string s;
 	    cin>>s;
 	    
-	    int power=0,index=0;
+	    long long power=0;
 	    int n=s.length();
-	    int b[26]={0};
-	    
-	    /*
-	    for(int i=0;i<s.length();i++)
-	    {
-	        
-	        cout<<s[i]-96<<" , ";
-	        cout<<i+1<<" , ";
-	        power = power +  ((i+1)* (s[i]-96));
-	        cout<<"^POWER^-  "<<power<<' , ';
-	        cout<<"(i+1)* (s[i]-96)____ "<<((i+1)* (s[i]-96))<<endl;
-	    }
-	    */
+	    int b[n];
 	    
 	    for(int i=0;i<n;i++)
-	    {
-	        index=(s[i]-96);
-	        b[i+1]++;
-	    }
-	    
-	    
-	    for(int i=0;i<26;i++)
-	    cout<<b[i]<<" , ";
-	    
+	        b[i]=s[i];
+	        
+	    sort(b, b+n);
 	    
 	    for(int i=0;i<s.length();i++)
 	    {
-	        
-	        if(b[i+1])
-	        {
-	        power = power +  (b[i+1] * (s[i]-96));
-	        }
-	        cout<<"^POWER^-  "<<power<<' , ';
-	        cout<<"b(i+1)*____ "<<b[i+1]<<endl;
-	        
+	        power +=  ((i+1)* (b[i]-96));
 	    }
-	    
 	    
 	    cout<<power<<endl;
 	   
