94e8969da3e89ee31dbf25e8a62fbbe4d25ef41e
Arpita Satsangi <107709451+ArpitaSatsangi@users.noreply.github.com>
2023-11-20 23:04:13 +0530

    Update 51. Two Pointers - Difference Pairs Difficulty Rating:  1000.cpp

--- 51. Two Pointers - Difference Pairs Difficulty Rating:  1000.cpp
+++ 51. Two Pointers - Difference Pairs Difficulty Rating:  1000.cpp
@@ -30,3 +30,43 @@ int main() {
 	}
 	return 0;
 }
+
+//----------------------------------------------------------------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	
+	int t;
+	cin>>t;
+	
+	while(t--)
+	{
+	    int n, b, i=0, j=1;
+	    bool flag = 0;
+	    cin>>n>>b;
+	    
+	    int a[n];
+	    for(int i=0;i<n;i++)
+	        cin>>a[i];
+	        
+	   while(i<j){
+	       
+	       if(abs(a[i]-a[j])==b)
+	           flag = 1;
+	           
+	       else if((a[j]-a[i]<b))
+                j++;
+            
+           else
+                i++;
+            
+	   }
+	   
+	   cout<<flag<<endl;
+	   
+	}
+	return 0;
+}
+	
